c50f871eb3a8a8caa855cd870012e38947566bcc
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2025-08-06 11:51:40 -0500

    Update NVM directory path in print.sh to use the new configuration location

--- pi/print.sh
+++ pi/print.sh
@@ -8,7 +8,7 @@ PROJECT_DIR="/home/trevor/Documents/todo-receipt-printer/pi"
 # --- END CONFIGURATION ---
 
 # Load NVM
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.config/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
 
 # Add a message to our log file to show the script has started
