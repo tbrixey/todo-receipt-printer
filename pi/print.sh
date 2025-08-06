d521f883f02eb7dd382a0b49e68e0708e1740110
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2025-08-06 11:34:46 -0500

    Add NVM support in print.sh to ensure the correct Node.js version is used for executing the application

--- pi/print.sh
+++ pi/print.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# Source NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Optional: Go to the project directory
 cd /home/trevor/Documents/todo-receipt-printer/pi
 
+nvm use default
 node dist/index.js
