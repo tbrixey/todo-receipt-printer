cdfad1cb8d09833f99b4eb4e184593782ed00201
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2025-08-06 12:20:55 -0500

    next ignore pi folder

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    config.watchOptions = {
+      ignored: "**/pi/**",
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
